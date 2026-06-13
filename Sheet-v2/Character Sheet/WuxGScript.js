f9629d7c289b8390222dfb793260fa8e51a49ef4
Vincent Fellows <vincentfellows@gmail.com>
2026-06-13 13:36:30 -0300

    Advancement tab added to character creation

--- Sheet-v2/Character Sheet/WuxGScript.js
+++ Sheet-v2/Character Sheet/WuxGScript.js
@@ -10573,7 +10573,7 @@ var WuxSheetNavigation = WuxSheetNavigation || (function () {
     };
     const buildCharacterCreationTabs = function (sheetName) {
         let output = "";
-        let tabNames = ["Gear", "Knowledge", "Styles", "Attributes", "Jobs", "Origin"];
+        let tabNames = ["Advancement", "Gear", "Knowledge", "Styles", "Attributes", "Jobs", "Origin"];
 
         for (let i = 0; i < tabNames.length; i++) {
             output += buildTabButton("radio", WuxDef.GetAttribute("Page"), tabNames[i], tabNames[i], tabNames[i] == sheetName, "") + "\n";
